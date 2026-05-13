f7ed028ea8be6bdd20af7abe63659ee9dfd635a2
renansouz <renanss2005@gmail.com>
2026-05-13 09:38:54 -0300

    update gitignore

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -99,7 +99,7 @@ export const Contact = () => {
           <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
             Let's build{" "}
             <span className="font-serif italic font-normal text-white">
-              something meaningful.
+              something together!
             </span>
           </h2>
           <p className="text-muted-foreground animate-fade-in animation-delay-200">
